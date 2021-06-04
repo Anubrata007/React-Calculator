92a6fa1cb0dc7ce3999db4d99a711ce78b487790
Anubrata007 <anubrata995@gmail.com>
2021-06-04 22:23:07 +0530

    React Calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [result, setResult] = useState("")
+
+    const Click = (e) => {
+        setResult(result.concat(e.target.name));
+    }
+
+    const Clear = () => {
+        setResult("")
+    }
+
+    const Backspace = () => {
+        setResult(result.slice(0, -1)); //(result.slice(0,result.length -1))
+    }
+
+    const Calculate = () => {
+        try {
+            setResult(eval(result).toString());
+        } catch (err) {
+            setResult("")
+        }
+    }
+    return (
+        <>
+            <div className="container">
+                <form>
+                    <input type="text" value={result} />
+                </form>
+
+                <div className="keypad">
+                    <button className="highlight" onClick={Clear} id="clear">Clear</button>
+                    <button className="highlight" onClick={Backspace} id="backspace">C</button>
+                    <button className="highlight" name="/" onClick={Click}>&divide;</button>
+                    <button name="7" onClick={Click}>7</button>
+                    <button name="8" onClick={Click}>8</button>
+                    <button name="9" onClick={Click}>9</button>
+                    <button className="highlight" name="*" onClick={Click}>&times;</button>
+                    <button name="4" onClick={Click}>4</button>
+                    <button name="5" onClick={Click}>5</button>
+                    <button name="6" onClick={Click}>6</button>
+                    <button className="highlight" name="-" onClick={Click}>&ndash;</button>
+                    <button name="1" onClick={Click}>1</button>
+                    <button name="2" onClick={Click}>2</button>
+                    <button name="3" onClick={Click}>3</button>
+                    <button className="highlight" name="+" onClick={Click}>+</button>
+                    <button name="0" onClick={Click}>0</button>
+                    <button name="." onClick={Click}>.</button>
+                    <button className="highlight" onClick={Calculate} id="result">=</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
